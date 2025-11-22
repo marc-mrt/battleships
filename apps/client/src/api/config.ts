6002a6173e890945fdf6def2b876b5bea3a5ef7d
Marc Morant <15051223+marc-mrt@users.noreply.github.com>
2025-11-22 21:49:29 +0100

    Fix websocket url definition

--- apps/client/src/api/config.ts
+++ apps/client/src/api/config.ts
@@ -1,26 +1,30 @@
-function getBaseUrl(): string {
-	const url = import.meta.env.VITE_SERVER_BASE_URL;
-	if (!url) {
+function getApiBaseUrl(): URL {
+	const urlRaw = import.meta.env.VITE_SERVER_BASE_URL;
+	if (!urlRaw) {
 		throw new Error('VITE_SERVER_BASE_URL is not defined.');
 	}
+
+	const url = new URL(urlRaw);
+	if (!url.protocol || !url.host) {
+		throw new Error(`Invalid VITE_SERVER_BASE_URL: ${urlRaw}`);
+	}
+
 	return url;
 }
 
-function isHttps(): boolean {
-	return window.location.protocol === 'https:';
+function getWebSocketProtocol(protocol: string): string {
+	return protocol === 'https:' ? 'wss' : 'ws';
 }
 
-function getWebSocketProtocol(): string {
-	return isHttps() ? 'wss' : 'ws';
+function getWebSocketUrl(apiBaseURL: URL): URL {
+	const protocol = getWebSocketProtocol(apiBaseURL.protocol);
+	const host = apiBaseURL.host;
+	const path = 'ws';
+
+	const url = new URL(`${protocol}://${host}/${path}`);
+	return url;
 }
 
-function getWebSocketUrl(baseURL: string): string {
-	const urlWithoutPath = baseURL.replace(/\/api$/, '');
-	const protocol = getWebSocketProtocol();
-	const wsUrl = urlWithoutPath.replace(/^http/, protocol);
-	return `${wsUrl}/ws`;
-}
+export const API_BASE_URL: URL = getApiBaseUrl();
 
-export const API_BASE_URL: string = getBaseUrl();
-
-export const WEBSOCKET_BASE_URL: string = getWebSocketUrl(API_BASE_URL);
+export const WEBSOCKET_BASE_URL: URL = getWebSocketUrl(API_BASE_URL);
